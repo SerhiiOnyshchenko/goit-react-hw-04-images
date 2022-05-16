d058b1806abbf9ee0530ce47bea571c06e71e465
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-05-16 16:44:28 +0300

    dz-4-3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import { useState, useEffect } from 'react';
 import ImageGallery from 'components/ImageGallery/ImageGallery';
 import Searchbar from 'components/Searchbar/Searchbar';
-import React, { Component } from 'react';
 import SearchApi from 'services/Api';
 import Notiflix from 'notiflix';
 import Button from 'components/Button/Button';
@@ -8,126 +8,103 @@ import Modal from 'components/Modal/Modal';
 import Loader from 'components/Loader/Loader';
 import * as Scroll from 'react-scroll';
 
-class App extends Component {
-   state = {
-      searchName: ' ',
-      countPage: 1,
-      per_page: 12,
-      ImagesList: [],
-      showModal: false,
-      showLoadMore: false,
-      loading: false,
-      openModalItem: { url: '', alt: '' },
-   };
+export default function App() {
+   const [searchName, setSearchName] = useState('');
+   const [countPage, setCountPage] = useState(1);
+   const [perPage] = useState(12);
+   const [imagesList, setImagesList] = useState([]);
+   const [showModal, setShowModal] = useState(false);
+   const [showLoadMore, setShowLoadMore] = useState(false);
+   const [loading, setLoading] = useState(false);
+   const [openModalItem, setOpenModalItem] = useState({ url: '', alt: '' });
 
-   componentDidUpdate(prevProps, prevState) {
-      const { searchName, per_page, countPage, ImagesList } = this.state;
-
-      if (
-         prevState.countPage !== countPage ||
-         prevState.searchName !== searchName
-      ) {
-         this.setState({ showLoadMore: false, loading: true });
-         SearchApi(searchName, countPage, per_page)
-            .then(date => {
-               const filterDataHits = date.hits.map(img => {
-                  return Object.fromEntries(
-                     Object.entries(img).filter(([key]) =>
-                        [
-                           'id',
-                           'tags',
-                           'largeImageURL',
-                           'webformatURL',
-                        ].includes(key)
-                     )
-                  );
-               });
-               this.setState(prev => ({
-                  ImagesList: [...prev.ImagesList, ...filterDataHits],
-                  totalHits: date.totalHits,
-                  loading: false,
-               }));
-               if (date.total !== date.hits.length) {
-                  this.setState({ showLoadMore: true });
-               }
-               if (countPage === 1) {
-                  Notiflix.Notify.success(
-                     `Hooray! We found ${date.totalHits} images.`
-                  );
-               }
-               if (date.total <= ImagesList.length + per_page) {
-                  this.setState({ showLoadMore: false });
-                  Notiflix.Notify.info(
-                     "We're sorry, but you've reached the end of search results."
-                  );
-               }
-            })
-            .catch(this.onApiError);
+   useEffect(() => {
+      if (!searchName) {
+         return;
       }
-   }
-   onApiError = () => {
+      setShowLoadMore(false);
+      setLoading(true);
+      SearchApi(searchName, countPage, perPage)
+         .then(date => {
+            const filterDataHits = date.hits.map(img => {
+               return Object.fromEntries(
+                  Object.entries(img).filter(([key]) =>
+                     ['id', 'tags', 'largeImageURL', 'webformatURL'].includes(
+                        key
+                     )
+                  )
+               );
+            });
+            setImagesList([...imagesList, ...filterDataHits]);
+            setLoading(false);
+            if (date.total !== date.hits.length) {
+               setShowLoadMore(true);
+            }
+            if (countPage === 1) {
+               Notiflix.Notify.success(
+                  `Hooray! We found ${date.totalHits} images.`
+               );
+            }
+            if (date.total <= imagesList.length + perPage) {
+               setShowLoadMore(false);
+               Notiflix.Notify.info(
+                  "We're sorry, but you've reached the end of search results."
+               );
+            }
+         })
+         .catch(onApiError);
+   }, [countPage, searchName]);
+
+   const onApiError = () => {
       Notiflix.Notify.failure(
          'Sorry, there are no images matching your search query. Please try again.'
       );
-      this.setState({ showLoadMore: false, loading: false });
+      setShowLoadMore(false);
+      setLoading(false);
    };
 
-   onSubmit = name => {
-      this.setState(prev =>
-         prev.searchName === name && prev.countPage === 1
-            ? { countPage: 1 }
-            : {
-                 searchName: name,
-                 countPage: 1,
-                 ImagesList: [],
-              }
-      );
+   const onSubmit = name => {
+      if (!name) {
+         Notiflix.Notify.failure('Please. Enter the name of the picture.');
+         setShowLoadMore(false);
+      }
+      if (searchName === name && countPage === 1) {
+         return;
+      }
+      setSearchName(name);
+      setCountPage(1);
+      setImagesList([]);
    };
 
-   onloadeMore = () => {
-      this.setState(prev => ({
-         countPage: prev.countPage + 1,
-      }));
-      this.scrollSlowly();
+   const onloadeMore = () => {
+      setCountPage(prev => prev + 1);
+      scrollSlowly();
    };
 
-   scrollSlowly = () => {
+   const scrollSlowly = () => {
       const { height: cardHeight } = document
          .querySelector('#ImageGallery')
          .firstElementChild.getBoundingClientRect();
       Scroll.animateScroll.scrollMore(cardHeight * 2);
    };
-   openModal = (url, alt) => {
-      const openModalItem = { url, alt };
-      this.setState({
-         showModal: true,
-         openModalItem,
-      });
+   const openModal = (url, alt) => {
+      setShowModal(true);
+      setOpenModalItem({ url, alt });
    };
-   closeModal = () => {
-      this.setState({ showModal: false });
-   };
-   render() {
-      const { ImagesList, showModal, openModalItem, showLoadMore, loading } =
-         this.state;
-      return (
-         <div className="App">
-            <Searchbar onSubmit={this.onSubmit} />
-            {showModal && (
-               <Modal
-                  url={openModalItem.url}
-                  alt={openModalItem.alt}
-                  onClose={this.closeModal}
-               />
-            )}
-            <ImageGallery params={ImagesList} openModal={this.openModal} />
-            {loading && <Loader />}
-            {showLoadMore && (
-               <Button onClick={this.onloadeMore} title="Load more" />
-            )}
-         </div>
-      );
-   }
-}
 
-export default App;
+   return (
+      <div className="App">
+         <Searchbar onSubmit={onSubmit} />
+         {showModal && (
+            <Modal
+               url={openModalItem.url}
+               alt={openModalItem.alt}
+               onClose={() => setShowModal(false)}
+            />
+         )}
+         <ImageGallery params={imagesList} openModal={openModal} />
+         {loading && <Loader />}
+         {showLoadMore && <Button onClick={onloadeMore} title="Load more" />}
+      </div>
+   );
+}
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,58 +1,56 @@
-import React, { Component } from 'react';
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import s from './Modal.module.css';
 import PropTypes from 'prop-types';
 
-const modalRoot = document.querySelector('#modal-root');
+export default function Modal({ onClose, url, alt }) {
+   const modalRoot = useRef(document.querySelector('#modal-root'));
 
-class Modal extends Component {
-   componentDidMount() {
-      window.addEventListener('keydown', this.handelKeyUp);
-   }
-   componentWillUnmount() {
-      window.removeEventListener('keydown', this.handelKeyUp);
-   }
-   handelKeyUp = e => {
+   useEffect(() => {
+      window.addEventListener('keydown', handelKeyUp);
+      return () => {
+         window.removeEventListener('keydown', handelKeyUp);
+      };
+   }, []);
+
+   const handelKeyUp = e => {
       if (e.code === 'Escape') {
-         this.addCloseClass();
+         addCloseClass();
          setTimeout(() => {
-            this.props.onClose();
+            onClose();
          }, 1000);
       }
    };
-   handleBackdropClick = e => {
+   const handleBackdropClick = e => {
       if (e.target === e.currentTarget) {
-         this.addCloseClass();
+         addCloseClass();
          setTimeout(() => {
-            this.props.onClose();
+            onClose();
          }, 1000);
       }
    };
-   addCloseClass = () => {
+   const addCloseClass = () => {
       const Overlay = document.querySelector('#CloseAnimateOverlay');
       const Modal = document.querySelector('#CloseAnimateModal');
       Overlay.classList.add(`${s.CloseAnimate}`);
       Modal.classList.add(`${s.CloseAnimate}`);
    };
-   render() {
-      const { url, alt } = this.props;
-      return createPortal(
-         <div
-            id="CloseAnimateOverlay"
-            className={s.Overlay}
-            onClick={this.handleBackdropClick}
-         >
-            <div id="CloseAnimateModal" className={s.Modal}>
-               <img src={url} alt={alt} />
-            </div>
-         </div>,
-         modalRoot
-      );
-   }
+   return createPortal(
+      <div
+         id="CloseAnimateOverlay"
+         className={s.Overlay}
+         onClick={handleBackdropClick}
+      >
+         <div id="CloseAnimateModal" className={s.Modal}>
+            <img src={url} alt={alt} />
+         </div>
+      </div>,
+      modalRoot.current
+   );
 }
+
 Modal.propTypes = {
    url: PropTypes.string.isRequired,
    alt: PropTypes.string.isRequired,
    onClose: PropTypes.func.isRequired,
 };
-export default Modal;
